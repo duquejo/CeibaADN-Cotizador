b80e4ba1398a0023ca14ad12677bc7d93b8f029e
Jose Miguel Duque Mejia - Ceiba Software <jose.duque@ceiba.com.co>
2021-12-30 11:44:15 -0500

    REF: TypeORM integration test approach

--- server/test/e2e/centrovacacional/controlador/controlador-centrovacacional.e2e-spec.ts
+++ server/test/e2e/centrovacacional/controlador/controlador-centrovacacional.e2e-spec.ts
@@ -1,229 +1,276 @@
 import * as request from 'supertest';
-import { Test } from '@nestjs/testing';
-import { createStubObj } from '../../../util/create-object.stub';
-import { HttpStatus, INestApplication } from '@nestjs/common';
-import { createSandbox, SinonStubbedInstance } from 'sinon';
+import { CentroVacacionalBuilder, CategoriaUsuariosBuilder, CalendarioFestivosBuilder } from '../../../util/test-builder';
+import { HttpStatus } from '@nestjs/common';
+import { axiosInstance } from '../../../../../client/src/app/core/config/AxiosConfig';
+import { CategoriaUsuariosEntidad } from '../../../../src/infraestructura/categoriausuarios/entidad/categoriausuarios.entidad';
+import { CalendarioFestivosEntidad } from '../../../../src/infraestructura/calendariofestivos/entidad/calendariofestivos.entidad';
+import { CentroVacacionalEntidad } from '../../../../src/infraestructura/centrovacacional/entidad/centrovacacional.entidad';
+import { connection, app } from 'test/util/test-connection';
 
-import { AppLogger } from '../../../../src/infraestructura/configuracion/ceiba-logger.service';
-import { FiltroExcepcionesDeNegocio } from '../../../../src/infraestructura/excepciones/filtro-excepciones-negocio';
+describe('Pruebas al controlador de Centros Vacacionales', () => {
 
-// Generales
-import { CategoriaUsuarios } from '../../../../../client/src/app/feature/Admin/models/CategoriaUsuarios';
-import { CentroVacacionalControlador } from '../../../../src/infraestructura/centrovacacional/controlador/centrovacacional.controlador';
-import { RepositorioCentroVacacional } from '../../../../src/dominio/centrovacacional/puerto/repositorio/repositorio-centrovacacional';
-import { RepositorioCalendarioFestivos } from '../../../../src/dominio/calendariofestivos/puerto/repositorio/repositorio-calendariofestivos';
+  let newCategory: CategoriaUsuariosEntidad;
+  let newCalendar: CalendarioFestivosEntidad;
+  let newCV: CentroVacacionalEntidad;
 
-// Leer
-import { DaoCentroVacacional } from '../../../../src/dominio/centrovacacional/puerto/dao/dao-centrovacacional';
-import { ManejadorObtenerCentroVacacional } from '../../../../src/aplicacion/centrovacacional/consulta/obtener-centrovacacional.manejador';
-
-// Guardar
-// import { ComandoGuardarCentroVacacional } from '../../../../src/aplicacion/centrovacacional/comando/guardar-centrovacacional.comando';
-import { ServicioGuardarCentroVacacional } from '../../../../src/dominio/centrovacacional/servicio/servicio-guardar-centrovacacional';
-import { ManejadorGuardarCentroVacacional } from '../../../../src/aplicacion/centrovacacional/comando/guardar-centrovacacional.manejador';
-import { servicioGuardarCentroVacacionalProveedor } from '../../../../src/infraestructura/centrovacacional/proveedor/servicio/servicio-guardar-centrovacacional.proveedor';
-
-// Actualizar
-import { ServicioActualizarCentroVacacional } from '../../../../src/dominio/centrovacacional/servicio/servicio-actualizar-centrovacacional';
-import { servicioActualizarCentroVacacionalProveedor } from '../../../../src/infraestructura/centrovacacional/proveedor/servicio/servicio-actualizar-centrovacacional.proveedor';
-import { ManejadorActualizarCentroVacacional } from '../../../../src/aplicacion/centrovacacional/comando/actualizar-centrovacacional.manejador';
-
-// Borrar
-import { ServicioBorrarCentroVacacional } from '../../../../src/dominio/centrovacacional/servicio/servicio-borrar-centrovacacional';
-import { ManejadorBorrarCentroVacacional } from '../../../../src/aplicacion/centrovacacional/comando/borrar-centrovacacional.manejador';
-import { servicioBorrarCentroVacacionalProveedor } from '../../../../src/infraestructura/centrovacacional/proveedor/servicio/servicio-borrar-centrovacacional.proveedor';
-import { RepositorioCategoriaUsuarios } from '../../../../src/dominio/categoriausuarios/puerto/repositorio/repositorio-categoriausuarios';
-import { CalendarioFestivos } from '../../../../src/dominio/calendariofestivos/modelo/calendariofestivos';
-
-/**
- * Un sandbox es util cuando el módulo de nest se configura una sola vez durante el ciclo completo de pruebas
- * */
-const sinonSandbox = createSandbox();
-
-describe('Pruebas al controlador del centro vacacional', () => {
-
-  let app: INestApplication;
-
-  let repositorioCentroVacacional: SinonStubbedInstance<RepositorioCentroVacacional>;
-  let daoCentroVacacional: SinonStubbedInstance<DaoCentroVacacional>;
-  
-  // Externos
-  let repositorioCalendarioFestivos: SinonStubbedInstance<RepositorioCalendarioFestivos>;
-  let repositorioCategoriaUsuarios: SinonStubbedInstance<RepositorioCategoriaUsuarios>;
-
-  /**
-   * No Inyectar los módulos completos (Se trae TypeORM y genera lentitud al levantar la prueba, traer una por una las dependencias)
-   **/
-  beforeAll( async () => {
-
-    repositorioCentroVacacional = createStubObj<RepositorioCentroVacacional>([
-      'guardar',
-      'existeCentroVacacional',
-      'existeNombreCentroVacacional',
-      'actualizar',
-      'borrar'
-    ], sinonSandbox);
-
-    repositorioCalendarioFestivos = createStubObj<RepositorioCalendarioFestivos>([
-      'validarCalendarios',
-    ], sinonSandbox);
-
-    repositorioCategoriaUsuarios = createStubObj<RepositorioCategoriaUsuarios>([
-      'validarCategorias',
-    ], sinonSandbox);
-
-    daoCentroVacacional         = createStubObj<DaoCentroVacacional>(['obtenerCentrosVacacionales'], sinonSandbox);
-    
-    const moduleRef = await Test.createTestingModule({
-      controllers: [ CentroVacacionalControlador ],
-      providers: [
-        AppLogger,
-        {
-          provide:    ServicioGuardarCentroVacacional,
-          inject:     [ RepositorioCentroVacacional, RepositorioCategoriaUsuarios, RepositorioCalendarioFestivos ],
-          useFactory: servicioGuardarCentroVacacionalProveedor,
-        },
-        {
-          provide:    ServicioActualizarCentroVacacional,
-          inject:     [ RepositorioCentroVacacional ],
-          useFactory: servicioActualizarCentroVacacionalProveedor,
-        },
-        {
-          provide:    ServicioBorrarCentroVacacional,
-          inject:     [ RepositorioCentroVacacional ],
-          useFactory: servicioBorrarCentroVacacionalProveedor,
-        },
-        { provide: RepositorioCentroVacacional, useValue: repositorioCentroVacacional },
-        { provide: RepositorioCategoriaUsuarios, useValue: repositorioCategoriaUsuarios },
-        { provide: RepositorioCalendarioFestivos, useValue: repositorioCalendarioFestivos },
-        { provide: DaoCentroVacacional, useValue: daoCentroVacacional },
-
-        /**
-         * CRUD Handlers
-         */
-        ManejadorGuardarCentroVacacional,
-        ManejadorObtenerCentroVacacional,
-        ManejadorActualizarCentroVacacional,
-        ManejadorBorrarCentroVacacional
-      ],
-    }).compile();
-
-    app = moduleRef.createNestApplication();
-
-    const logger = await app.resolve( AppLogger );
-    logger.customError = sinonSandbox.stub();
-
-    app.useGlobalFilters( new FiltroExcepcionesDeNegocio( logger ) );
-
-    await app.init();
+  beforeAll(async () => {
+    await connection.create();
   });
 
-  afterEach( () => sinonSandbox.restore() );
+  afterAll(async () => {
+    await connection.close();
+  });
 
-  afterAll( async () => await app.close() );
-
-  // it('Debería crear un centro vacacional de manera exitosa', async () => {
-
-  //   const centroVacacionalBaseData: any = {
-  //     nombre: 'Parque Tayrona',
-  //     descripcion: 'Aventúrate con tu familia',
-  //     calendarios: [ 1,2,3 ],
-  //     categoriaUsuarios: [1,2,3 ],
-  //     calendarioActivo: 3
-  //   };
-
-  //   const calendariosFestivosMock = [{
-  //     id: 1,
-  //     nombre: 'Campaña empleados Diciembre', // Nombre
-  //     descripcion: 'Campaña para empleados aguinaldo navideño', // Descripción
-  //     festivos: [ '2021-12-18' ] // Festivos
-  //   }] as CalendarioFestivosEntidad[];
-
-  //   repositorioCentroVacacional.existeNombreCentroVacacional.returns( Promise.resolve( false ) );
-    
-  //   // // Calendarios válidos ( Uno válido por ej )
-  //   repositorioCalendarioFestivos.validarCalendarios.returns( Promise.resolve([ calendariosFestivosMock,2]) );
-    
-  //   // // Categorías válidas ( Dos válidas por ej )
-  //   // repositorioCategoriaUsuarios.validarCategorias.returns( Promise.resolve([[],2]) );
-
-  //   const response = await request( app.getHttpServer() ).post('/centrosVacacionales')
-  //     .send( centroVacacionalBaseData )
-  //     .expect( HttpStatus.CREATED );
-  // });
-
-  it('Debería obtener los centros vacacionales almacenadas', () => {
+  beforeEach(async () => {
+    await connection.clear();  
 
     /**
-     * Mock dependencias
+     * 
+     * Categoría de usuarios
      */
-    const calendarioDataMock = {
-      nombre: 'Campaña empleados Diciembre', // Nombre
-      descripcion: 'Campaña para empleados aguinaldo navideño', // Descripción
-      festivos: [ '2021-12-18' ] // Festivos
-    } as CalendarioFestivos;
+    const _categoriaUsuarios = new CategoriaUsuariosBuilder(
+      'Categoría de asociados de prueba', 50000, 45000
+    );
 
-    const categoriaUsuariosDataMock = {
-      id: 1,
-      nombre: "Menor a $800.000 COP",
-      descripcion: "Cartagena",
-      valorAlta: 50000,
-      valorBaja: 25000,
-      fechaActualizacion: new Date()
-    } as CategoriaUsuarios;    
+    try {
+      const { data: respuestaCategoriaUsuarios } = await axiosInstance.post( 
+        `${ app }/categoriasUsuarios`, {
+        nombre: _categoriaUsuarios.nombre,
+        valorAlta: _categoriaUsuarios.valorAlta,
+        valorBaja: _categoriaUsuarios.valorBaja
+      });
+      newCategory = respuestaCategoriaUsuarios;  
+    } catch (error) {
+      throw new Error(`Error generando categoría de usuario de pruebas: ${ error }`);
+    }
 
-    const centroVacacionalBaseData: any[] = [{
-      nombre: 'Parque Tayrona',
-      descripcion: 'Aventúrate con tu familia',
-      categoriaUsuarios: [ categoriaUsuariosDataMock ],
-      calendarios: [ calendarioDataMock ],
-      calendarioActivo: 3
-    }];
+    /**
+     * Calendarios
+     */
+    const _calendarioFestivos = new CalendarioFestivosBuilder(
+      'Campaña empleados Diciembre'
+    );
 
-    daoCentroVacacional.obtenerCentrosVacacionales.returns( Promise.resolve( centroVacacionalBaseData ) );
+    try {
+      const { data: respuestaCalendarioFestivos } = await axiosInstance.post( 
+        `${ app }/calendariosFestivos`, {
+        nombre: _calendarioFestivos.nombre
+      });  
+      newCalendar = respuestaCalendarioFestivos;
+    } catch (error) {
+      throw new Error(`Error generando calendario de pruebas: ${ error }`);
+    }
+    
 
-    return request( app.getHttpServer() ).get( '/centrosVacacionales' )
-      .expect( HttpStatus.OK );
+    /**
+     * 
+     * Centro Vacacional
+     */
+     const _centroVacacional = new CentroVacacionalBuilder(
+      'Categoría de asociados de prueba',
+    )
+    .setDescripcion( 'Descripción del centro vacacional de prueba' )
+    .setCalendarios( [ newCalendar.id ] )
+    .setCategoriasUsuarios( [ newCategory.id ] )
+    .setCalendarioActivo( newCalendar.id );
+
+    try {
+      const { data: respuestaCentroVacacional } = await axiosInstance.post( 
+        `${ app }/centrosVacacionales`, {
+        nombre: _centroVacacional.nombre,
+        descripcion: _centroVacacional.descripcion,
+        calendarios: _centroVacacional.calendarios,
+        categoriaUsuarios: _centroVacacional.categoriasUsuarios,
+        calendarioActivo: _centroVacacional.calendarioActivo,
+      });      
+      newCV = respuestaCentroVacacional;
+    } catch( error ) {
+      throw new Error(`Error generando centro vacacional de prueba: ${ error }`);
+    }
   });
 
-  it('Debería fallar al intentar borrar un centro vacacional inexistente', async () => {
+  /**
+   * @TODO falta obtener
+   */
+  it('Debería guardar un centro vacacional de manera exitosa', () => {
 
     // Arrange
-    const centroVacacionalId = 1;
+    const _calendario = newCalendar;
+    const _categoria = newCategory;
+    const _centroVacacional = new CentroVacacionalBuilder(
+      'Centro Vacacional de prueba'
+    )
+    .setDescripcion( 'Descripción del centro vacacional de prueba' )
+    .setCalendarios( [ _calendario.id ] )
+    .setCategoriasUsuarios( [ _categoria.id ] )
+    .setCalendarioActivo( _calendario.id );
 
-    repositorioCentroVacacional.existeCentroVacacional.returns( Promise.resolve( [[], 0]) );
-
-    const response = await request( app.getHttpServer() )
-      .delete( `/centrosVacacionales/${ centroVacacionalId }`)
-      .expect( HttpStatus.NOT_FOUND );
-
-    expect(response.body.message).toBe( `El centro vacacional {${ centroVacacionalId }} no existe` );
-    expect(response.body.statusCode).toBe(HttpStatus.NOT_FOUND);
+    // Act & Assert
+    return request( app ).post( '/centrosVacacionales' )
+    .set('Accept', 'application/json')
+    .send({
+      nombre: _centroVacacional.nombre,
+      descripcion: _centroVacacional.descripcion,
+      calendarios: _centroVacacional.calendarios,
+      categoriaUsuarios: _centroVacacional.categoriasUsuarios,
+      calendarioActivo: _centroVacacional.calendarioActivo,
+    })
+    .expect( ({ body }: request.Response ) => {
+      expect( body.id ).toBeDefined();
+      expect( body.fechaCreacion ).toBeDefined();
+      expect( body.fechaActualizacion ).toBeDefined();
+      expect( body.nombre ).toBe( _centroVacacional.nombre );
+      expect( body.descripcion ).toBe( _centroVacacional.descripcion );
+      expect( body.calendarios ).toEqual( [ _calendario ] );
+      expect( body.categoriaUsuarios ).toEqual( [ _categoria ] );
+      expect( body.calendarioActivo ).toBe( _centroVacacional.calendarioActivo );
+    })
+    .expect( HttpStatus.CREATED );
   });
 
-  // it('Debería fallar al actualizar un centro vacacional que no existe', async () => {
+  it('Debería guardar un centro vacacional asignando por calendario activo el primero si se envía null', () => {
 
-  //   const centroVacacionalBaseData: ComandoActualizarCentroVacacional = {
-  //     nombre: 'Parque Tayrona',
-  //     descripcion: 'Aventúrate con tu familia',
-  //     categoriaUsuarios: categoriaUsuariosDataMock,
-  //     calendarios: calendarioDataMock,
-  //     calendarioActivo: 3
-  //   };
+    // Arrange
+    const _calendario = newCalendar;
+    const _categoria = newCategory;
+    const _centroVacacional = new CentroVacacionalBuilder(
+      'Centro vacacional de prueba con calendario activo asignado'
+    )
+    .setCalendarios( [ _calendario.id ] )
+    .setCategoriasUsuarios( [ _categoria.id ] )
 
-  //   const centroVacacionalId = 1;
+    // Act & Assert
+    return request( app ).post( '/centrosVacacionales' )
+    .set('Accept', 'application/json')
+    .send({
+      nombre: _centroVacacional.nombre,
+      calendarios: _centroVacacional.calendarios,
+      categoriaUsuarios: _centroVacacional.categoriasUsuarios
+    })
+    .expect( ({ body }: request.Response ) => {
+      expect( body.nombre ).toBe( _centroVacacional.nombre );
+      expect( body.calendarioActivo ).toBe( _calendario.id );
+    })
+    .expect( HttpStatus.CREATED );
+  });
 
-  //   repositorioCentroVacacional.existeCentroVacacional.returns( Promise.resolve( [[],1]) );
-    
-  //   // Calendarios válidos ( Uno válido por ej )
-  //   repositorioCalendarioFestivos.validarCalendarios.returns( Promise.resolve([[],1]) );
-    
-  //   // Categorías válidas ( Dos válidas por ej )
-  //   repositorioCategoriaUsuarios.validarCategorias.returns( Promise.resolve([[],2]) );    
+  it('Debería permitir crear un centro vacacional con la información opcional', () => {
 
-  //   const response = await request( app.getHttpServer() )
-  //     .patch( `/centrosVacacionales/${ centroVacacionalId }`)
-  //     .send( centroVacacionalBaseData )
-  //     .expect( HttpStatus.NOT_FOUND );
-  // }); 
+    // Arrange 
+    const _centroVacacional = new CentroVacacionalBuilder(
+      'Centro vacacional de prueba con información por defecto'
+    );
+
+    // Act & Assert
+    return request( app ).post( '/centrosVacacionales' )
+    .set('Accept', 'application/json')
+    .send({ nombre: _centroVacacional.nombre })
+    .expect( ({ body }: request.Response ) => {
+      expect( body.nombre ).toBe( _centroVacacional.nombre );
+      expect( body.calendarioActivo ).toBeNull();
+    })
+    .expect( HttpStatus.CREATED );    
+  });
+
+  it('Debería fallar al intentar crear un centro vacacional con nombre existente', () => {
+
+    // Arrange 
+    const centroVacacionalExistente = newCV;
+    const _centroVacacional = new CentroVacacionalBuilder(
+      centroVacacionalExistente.nombre
+    );
+
+    // Act & Assert
+    return request( app ).post( '/centrosVacacionales' )
+    .set('Accept', 'application/json')
+    .send({ nombre: _centroVacacional.nombre })
+    .expect( HttpStatus.BAD_REQUEST );
+  });  
+
+  it('Debería fallar al crear un centro vacacional cuyo calendario no existe', () => {
+
+    // Arrange 
+    const _centroVacacional = new CentroVacacionalBuilder(
+      'Centro vacacional de prueba con calendario que no existe'
+    ).setCalendarios( [ 1000, 1200 ] );
+
+    // Act & Assert
+    return request( app ).post( '/centrosVacacionales' )
+    .set('Accept', 'application/json')
+    .send({ 
+      nombre: _centroVacacional.nombre,
+      calendarios: _centroVacacional.calendarios
+    })
+    .expect( HttpStatus.UNPROCESSABLE_ENTITY );
+  });
+
+  it('Debería fallar al crear un centro vacacional cuya categoría de usuarios no existe', () => {
+
+    // Arrange 
+    const newCentroVacacionalObj = newCV;
+    const _centroVacacional = new CentroVacacionalBuilder(
+      'Centro vacacional de prueba con categorías que no existen'
+    ).setCategoriasUsuarios( [ 2000, 4000 ] );
+
+    // Act & Assert
+    return request( app ).post( '/centrosVacacionales' )
+    .set('Accept', 'application/json')
+    .send({ 
+      nombre: _centroVacacional.nombre,
+      categoriaUsuarios: _centroVacacional.categoriasUsuarios
+    })
+    .expect( HttpStatus.UNPROCESSABLE_ENTITY );
+  });
+
+  it('Debería actualizar un centro vacacional existente', () => {
+    // Arrange
+    const newCentroVacacionalObj = newCV;
+    const _centroVacacional = new CentroVacacionalBuilder(
+      'Centro vacacional modificado'
+    );
+
+    // Act & Assert
+    return request( app )
+    .patch( `/centrosVacacionales/${ newCentroVacacionalObj.id }`)
+    .send({
+      nombre: _centroVacacional.nombre
+    })
+    .expect( HttpStatus.OK );
+  });
+
+  it('Debería fallar al actualizar un centro vacacional que no existe', () => {
+    // Arrange
+    const centroVacacionalId = 10000;
+    const _centroVacacional = new CentroVacacionalBuilder(
+      'Centro vacacional de prueba con información por defecto'
+    ).setNombre( 'Centro vacacional modificado' );
+
+    // Act & Assert
+    return request( app )
+    .patch( `/centrosVacacionales/${ centroVacacionalId }`)
+    .send({
+      nombre: _centroVacacional.nombre
+    })
+    .expect( HttpStatus.NOT_FOUND );
+  });
+
+  it('Debería borrar un centro vacacional existente', () => {
+    // Arrange
+    const newCentroVacacionalObj = newCV;
+
+    // Act & Assert
+    return request( app )
+    .delete( `/centrosVacacionales/${ newCentroVacacionalObj.id }`)
+    .expect( HttpStatus.OK );
+  });
+
+  it('Debería fallar al borrar un centro vacacional que no existe', () => {
+    // Arrange
+    const centroVacacionalId = 100000;
+
+    // Act & Assert
+    return request( app )
+    .delete( `/centrosVacacionales/${ centroVacacionalId }`)
+    .expect( HttpStatus.NOT_FOUND );
+  });
 });
--- server/test/util/test-builder.ts
+++ server/test/util/test-builder.ts
@@ -1,10 +1,14 @@
-import { CalendarioFestivos } from '../../src/dominio/calendariofestivos/modelo/calendariofestivos';
+import * as moment from 'moment';
+import { CategoriaUsuarios } from 'src/dominio/categoriausuarios/modelo/categoriausuarios';
+import { CalendarioFestivos } from 'src/dominio/calendariofestivos/modelo/calendariofestivos';
+import { CentroVacacional } from 'src/dominio/centrovacacional/modelo/centrovacacional';
 
 /**
+ * 
  * Test Builder Pattern
+ * 
  */
- export class CalendarioFestivosBuilder {
-
+export class CalendarioFestivosBuilder {
     private _nombre: string;
     private _descripcion = '';
     private _festivos: string[] = [];
@@ -28,8 +32,25 @@ import { CalendarioFestivos } from '../../src/dominio/calendariofestivos/modelo/
         return this;
     }
 
-    build() : CalendarioFestivos {
-        return new CalendarioFestivos( this._nombre, this._descripcion, this._festivos );
+    /**
+     * Retorna los festivos en formato moment
+     * @returns 
+     */
+    setFestivosMoment(): CalendarioFestivosBuilder {
+        this._festivos = this._festivos.map( ( festivo: string ) => moment( festivo ).format() );
+        return this;
+    }
+
+    /**
+     * Construye un objeto del tipo CalendarioFestivos
+     * @returns {CalendarioFestivos}
+     */
+    build(): CalendarioFestivos {
+        return new CalendarioFestivos(
+            this._nombre,
+            this._descripcion,
+            this._festivos 
+        );
     }
 
     get nombre() {
@@ -42,5 +63,138 @@ import { CalendarioFestivos } from '../../src/dominio/calendariofestivos/modelo/
 
     get festivos() {
         return this._festivos; 
-    }   
+    }
+}
+
+export class CategoriaUsuariosBuilder {
+    private _nombre: string;
+    private _descripcion: string = '';
+    private _valorAlta = 0;
+    private _valorBaja = 0;
+
+    constructor( nombre: string, valorAlta: number, valorBaja: number ) {
+        this._nombre = nombre;
+        this._valorAlta = valorAlta;
+        this._valorBaja = valorBaja;
+    }
+
+    setNombre( nombre: string ): CategoriaUsuariosBuilder {
+        this._nombre = nombre; 
+        return this;
+    }
+
+    setDescripcion( descripcion: string ): CategoriaUsuariosBuilder {
+        this._descripcion = descripcion;
+        return this;
+    }
+
+    setValorAlta( valorAlta: number ): CategoriaUsuariosBuilder {
+        this._valorAlta = valorAlta; 
+        return this;
+    }
+
+    setValorBaja( valorBaja: number ): CategoriaUsuariosBuilder {
+        this._valorBaja = valorBaja; 
+        return this;
+    }
+
+    /**
+     * Construye un objeto del tipo CalendarioFestivos
+     * @returns {CategoriaUsuarios}
+     */
+    build(): CategoriaUsuarios {
+        return new CategoriaUsuarios(
+            this._nombre,
+            this._descripcion,
+            this._valorAlta,
+            this._valorBaja,
+        );
+    }
+
+    get nombre() {
+        return this._nombre; 
+    }
+
+    get descripcion() {
+        return this._descripcion; 
+    }
+
+    get valorAlta() {
+        return this._valorAlta; 
+    }    
+
+    get valorBaja() {
+        return this._valorBaja; 
+    }
+}
+
+export class CentroVacacionalBuilder {
+    private _nombre: string;
+    private _descripcion: string = '';
+    private _calendarioActivo?: number = null;
+    private _calendarios: number[] = [];
+    private _categoriasUsuarios: number[] = [];
+
+    constructor( nombre: string ) {
+        this._nombre = nombre;
+    }
+
+    setNombre( nombre: string ): CentroVacacionalBuilder {
+        this._nombre = nombre; 
+        return this;
+    }
+
+    setDescripcion( descripcion: string ): CentroVacacionalBuilder {
+        this._descripcion = descripcion;
+        return this;
+    }
+
+    setCalendarioActivo( calendarioActivo: number ): CentroVacacionalBuilder {
+        this._calendarioActivo = calendarioActivo; 
+        return this;
+    }
+
+    setCalendarios( calendarios: number[] ): CentroVacacionalBuilder {
+        this._calendarios = calendarios; 
+        return this;
+    }
+
+    setCategoriasUsuarios( categoriasUsuarios: number[] ): CentroVacacionalBuilder {
+        this._categoriasUsuarios = categoriasUsuarios; 
+        return this;
+    }
+
+    /**
+     * Construye un objeto del tipo CentroVacacional
+     * @returns {CentroVacacional}
+     */
+    build(): CentroVacacional {
+        return new CentroVacacional(
+            this._nombre,
+            this._descripcion,
+            this._calendarios,
+            this._categoriasUsuarios,
+            this._calendarioActivo
+        );
+    }
+
+    get nombre() {
+        return this._nombre; 
+    }
+
+    get descripcion() {
+        return this._descripcion; 
+    }
+
+    get calendarioActivo() {
+        return this._calendarioActivo; 
+    }
+
+    get calendarios() {
+        return this._calendarios; 
+    }
+
+    get categoriasUsuarios() {
+        return this._categoriasUsuarios; 
+    }
 }
